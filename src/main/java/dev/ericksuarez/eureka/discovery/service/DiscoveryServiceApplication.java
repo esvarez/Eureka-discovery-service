ae2e79266c68ef173f74c25ba209480dd9c215c4
svarez <ericksuarezdev@gmail.com>
2019-11-06 00:41:17 -0600

    Service done

---
+++ src/main/java/dev/ericksuarez/eureka/discovery/service/DiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.ericksuarez.eureka.discovery.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
